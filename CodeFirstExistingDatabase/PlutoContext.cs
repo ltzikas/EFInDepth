a2aec6dbbdd6d7fe2ec2689baa0766f6caefae79
ltzikas <lucio.tzikas@gmail.com>
2023-04-24 02:40:36 -0300

    End of Section 4

--- CodeFirstExistingDatabase/PlutoContext.cs
+++ CodeFirstExistingDatabase/PlutoContext.cs
@@ -14,7 +14,7 @@ namespace CodeFirstExistingDatabase
 
         public virtual DbSet<Author> Authors { get; set; }
         public virtual DbSet<Course> Courses { get; set; }
-        public virtual DbSet<Tag> Tags { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }        
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
